ce07d42b4dd828fb3b5de36869eef8ef1b0bd094
Stiverson Machado <stiverson.machado@gmail.com>
2024-08-26 22:23:49 -0300

    Adição arquivo aula 3 tipo de tipagem

---
+++ aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Tives Machado')
+print(1, 'Tives "Machado"')
+
+# Aspas duplas
+print("Tives Machado")
+print(2, "Tives 'Machado'")
+
+# Escape
+print("tives \"Machado\"")
+
+# r
+print(r"Tives \"Machado\"")

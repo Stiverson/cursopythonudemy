0a8cea26bc9284d0545cbbdca8e2013c3e488bfb
Stiverson Machado <stiverson.machado@gmail.com>
2024-08-26 22:02:07 -0300

    dados Iniciais das primeiras aulas comentários e separadores

---
+++ aula1.py
@@ -0,0 +1,14 @@
+# Comando para escrever comentário
+print(123) # Na frente
+# Abaixo 
+print(567)
+"""
+DocString
+Escreve o conteúdo que eu quiser 
+acrescentar
+ex: explicar o código e etc..
+"""
+
+'''Usado para escrever minhas notas'''
+
+print("Texto sem erro heauiaehiuea")

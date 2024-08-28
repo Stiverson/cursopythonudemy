80ceeb9dda0aae5722dfba2c6f9b66e0c327757e
Stiverson Machado <stiverson.machado@gmail.com>
2024-08-27 22:01:58 -0300

    Adição das aulas 4 á aula 8 inclusão de exercícios, tipos e variaveis

---
+++ aula8.py
@@ -0,0 +1,15 @@
+nome = 'Stiverson'
+sobrenome = 'Machado'
+idade = 37
+maior_de_idade = idade >= 18
+ano_nascimento = 2024-idade
+altura_metros = 1.68
+
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_nascimento)
+print('E maior de idade?:', maior_de_idade)
+print('Altura Em Metros:', altura_metros)
+

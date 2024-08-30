2f02a09368624a6011d0633be12cec66e46da779
Stiverson Machado <stiverson.machado@gmail.com>
2024-08-29 21:13:21 -0300

    Organização do primeiro módulo de estudo e continuação do aprendizado da linguagem.

---
+++ Modulo-1- python iniciante/aula10.py
@@ -0,0 +1,7 @@
+concatenacao = 'Stiverson' + ' ' + 'Machado'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_tives = 3 * 'Tives'
+print(a_dez_vezes)
+print(tres_vezes_tives)
---
+++ Modulo-1- python iniciante/aula12.py
@@ -0,0 +1,8 @@
+nome = 'Stiverson Machado'
+altura = 1.68
+peso = 75
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
